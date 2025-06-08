731b542cbf052a1bb8b8aceafd58815b6be79b36
alrenoteD <just3ofuz@gmail.com>
2025-06-08 16:14:01 -0300

    Update tts_api.py

--- tts_api.py
+++ tts_api.py
@@ -2,15 +2,24 @@ from flask import Flask, request, send_file
 import subprocess, uuid, os, tempfile
 
 app = Flask(__name__)
-MODEL = "/opt/piper/models/edresson.onnx"
+
+MODELS = {
+    "feminino": "/opt/piper/models/pt_BR-faber-medium.onnx",
+    "masculino": "/opt/piper/models/pt_BR-edresson-low.onnx"
+}
 
 @app.route("/tts", methods=["POST"])
 def tts():
     text = request.json.get("text", "")
+    voice = request.json.get("voice", "feminino").lower()
+
+    model_path = MODELS.get(voice, MODELS["feminino"])
     tmp_wav = os.path.join(tempfile.gettempdir(), f"{uuid.uuid4()}.wav")
+
     subprocess.run(
-        f'echo "{text}" | piper --model {MODEL} --output_file {tmp_wav}',
+        f'echo "{text}" | piper --model {model_path} --output_file {tmp_wav}',
         shell=True, check=True)
+    
     return send_file(tmp_wav, mimetype="audio/wav")
 
 if __name__ == "__main__":
